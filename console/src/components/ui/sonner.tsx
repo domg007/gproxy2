cdec4673be5a76891b79eba424154ce0c66bc46f
LeenHawk <leenhawk@leenhawk.com>
2026-06-13 02:01:02 +0800

    style(console): restore props indentation in sonner.tsx

--- console/src/components/ui/sonner.tsx
+++ console/src/components/ui/sonner.tsx
@@ -34,7 +34,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
           "--border-radius": "var(--radius)",
         } as React.CSSProperties
       }
-{...props}
+      {...props}
     />
   )
 }
